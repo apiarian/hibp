c3a495b96973f150d00acc1888aa8c6e0920f315
Aleksandr Pasechnik <al@megamicron.net>
2018-03-04 07:11:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HIBP (Have I Been Pwned)
+
+A command-line password checker built on top of the
+https://github.com/mattevans/pwned-passwords library.
+
+Install with `go get -u github.com/apiarian/hibp`.
+
+See `hibp -help` for usage information.
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/mattevans/pwned-passwords"
+)
+
+func main() {
+	var (
+		help  bool
+		show  bool
+		stdin bool
+	)
+
+	flag.BoolVar(&help, "help", false, "Show help and exit.")
+	flag.BoolVar(&show, "show", false, "Do not hide the password on input (default false).")
+	flag.BoolVar(&stdin, "stdin", false, "Read passwords from STDIN instead of a prompt, one password per line, detects if *any* password is compromised.")
+
+	flag.Parse()
+
+	if help {
+		fmt.Println(`Check password against the https://haveibeenpwned.com/Passwords API.
+Use -show to echo the password at the prompt.
+Use -stdin to check more than one password at once. Does not indicate which password is compromised, only that at least one is compromised.
+Exits with the status code 2 if a password was found to be compromised.`)
+		os.Exit(0)
+	}
+
+	client := hibp.NewClient()
+
+	if stdin {
+		scanner := bufio.NewScanner(os.Stdin)
+
+		var pwned bool
+		for scanner.Scan() {
+			if !pwned {
+				// We scan all of STDIN up to EOF, but we don't actually check
+				// passwords once we've found one that is compromised.
+
+				p, err := client.Pwned.Compromised(scanner.Text())
+				if err != nil {
+					fmt.Printf("failed to check password: %v\n", err)
+					os.Exit(1)
+				}
+				if p {
+					pwned = true
+				}
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("failed to stand STDIN: %v\n", err)
+			os.Exit(1)
+		}
+
+		if pwned {
+			fmt.Println("at least one password is comporomised")
+			os.Exit(2)
+		} else {
+			fmt.Println("no compromised passwords found")
+			os.Exit(0)
+		}
+
+	} else {
+		var (
+			line string
+			err  error
+		)
+		if show {
+			line, err = readLine("password: ")
+		} else {
+			line, err = readSecretLine("password: ")
+		}
+		if err != nil {
+			fmt.Printf("failed to read password: %v\n", err)
+			os.Exit(1)
+		}
+
+		pwned, err := client.Pwned.Compromised(line)
+		if err != nil {
+			fmt.Printf("failed to check password: %v\n", err)
+		}
+
+		if pwned {
+			fmt.Print("that password is compromised\n")
+			os.Exit(2)
+		} else {
+			fmt.Print("that password is not compromised\n")
+			os.Exit(0)
+		}
+	}
+}
+
+func readLine(prompt string) (string, error) {
+	fmt.Print(prompt)
+
+	s, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSuffix(s, "\n"), nil
+}
+
+func readSecretLine(prompt string) (string, error) {
+	fmt.Print(prompt)
+
+	b, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println()
+
+	return strings.TrimSuffix(string(b), "\n"), nil
+}
